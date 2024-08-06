f0822cc150bbdc44503d116ab209ac9c5a658e5a
Gabriela Yoncheva <g.yoncheva1@gmail.com>
2024-08-06 18:13:28 +0300

    Test the local git username/email setup.

--- src/main/java/com/currency/gateway/service/CurrencyXmlApiService.java
+++ src/main/java/com/currency/gateway/service/CurrencyXmlApiService.java
@@ -1,8 +1,14 @@
 package com.currency.gateway.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Service for xml API requests.
  */
+@Service
+@Slf4j
 public class CurrencyXmlApiService {
     //TODO:
 }
